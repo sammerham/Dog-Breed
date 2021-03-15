49f941b408fa07f65533eb0cddd8177484efe760
Sam Merham <samehisaac@yahoo.com>
2021-03-14 21:20:26 -0700

    first init

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('#form');
+const btn = document.querySelector('#submit');
+
+
+async function getRandomDog() {
+    const response = await axios.get('https://dog.ceo/api/breeds/image/random');
+    const img = response.data.message;
+    image.src = img;
+}
+
+
+async function getDogByBreed(breed) {
+
+    try {
+        const url = `https://dog.ceo/api/breed/${breed}/images/random`;
+        const response = await axios.get(url);
+        const image = document.querySelector('#image');
+        image.src = response.data.message;
+    } catch (e) {
+        alert('hey BOI!!! BREED NOT FOUND ')
+        getRandomDog();
+    }
+    
+}
+
+form.addEventListener('submit',function(e){
+    const input = document.querySelector('#input');
+    e.preventDefault();
+    getDogByBreed(input.value);
+    input.value = '';
+})
